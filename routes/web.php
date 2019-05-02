1a7a68f27da7c908720960894e0f0177711b9a09
Nick <zx2581396@gmail.com>
2019-05-02 14:38:23 +0800

    Add mission route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('mission', 'MissionController@index')->name('mission');
+Route::post('mission', 'MissionController@createMission');
